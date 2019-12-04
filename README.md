5bf8b94d737b094294fd07aaeb47d181d1391d77
Nematjon <frank.sport.93@gmail.com>
2019-12-04 08:32:39 +0300

    adding files for github

---
+++ README.md
@@ -0,0 +1,3 @@
+# Profile
+
+Profile of web developer - Nematjon Abdulloev.
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+
+function App(props) {
+  return (
+    <div className="app">
+      <h1 className="">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
