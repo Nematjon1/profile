248e7f85ca380717a019b5ec7b80b61fd8f4b3e3
Nematjon <frank.sport.93@gmail.com>
2019-12-04 08:29:22 +0300

    Updates

---
+++ static/js/main.dd60f1ef.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpprofile=this.webpackJsonpprofile||[]).push([[0],[,,,function(e,n,t){e.exports=t(10)},,,,,function(e,n,t){},function(e,n,t){},function(e,n,t){"use strict";t.r(n);var a=t(0),r=t.n(a),o=t(2),c=t.n(o);t(8),t(9);var l=function(e){return r.a.createElement("div",{className:"app"},r.a.createElement("h1",{className:""},"Hello world!"))};c.a.render(r.a.createElement(l,null),document.getElementById("root"))}],[[3,1,2]]]);
+//# sourceMappingURL=main.dd60f1ef.chunk.js.map
