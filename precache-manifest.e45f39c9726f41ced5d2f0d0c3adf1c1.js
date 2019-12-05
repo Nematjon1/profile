d0e8ee06973d98db49688eceb8e513225665c51b
Nematjon <frank.sport.93@gmail.com>
2019-12-05 09:40:33 +0300

    Updates

---
+++ precache-manifest.e45f39c9726f41ced5d2f0d0c3adf1c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "801e6f61e6fb6188ee84e4cd6989b00a",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "ff25025e3aba93a44779",
+    "url": "/profile/static/css/main.db9f60c5.chunk.css"
+  },
+  {
+    "revision": "83114736bafc850b6706",
+    "url": "/profile/static/js/2.5d356652.chunk.js"
+  },
+  {
+    "revision": "ff25025e3aba93a44779",
+    "url": "/profile/static/js/main.ac5947cf.chunk.js"
+  },
+  {
+    "revision": "6f713858d12a66f89be2",
+    "url": "/profile/static/js/runtime-main.715cbc93.js"
+  }
+]);
